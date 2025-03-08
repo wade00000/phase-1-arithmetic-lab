84fb2263e8090f03b46c5459e7022697ac464191
WADE <MEANTOINE12@GMAIL.COM>
2025-03-08 19:03:57 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 1
+let num2 = 62
+let num3 = 24
+let num4 = 5
 
-// Write your code here
+const multiply = num1*num2
+
+const random = Math.ceil((Math.random()*100)+ 1)
+
+const mod = num3 % num4
+
+const max = Math.max(2,3,5,7,11,13,17,20)
